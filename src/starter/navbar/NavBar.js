009e7adcb1fbd2754aafb4dd36cdd02fc2a84edb
matohoundev <matohoundev@gmail.com>
2022-11-23 17:28:33 +0100

    add change link navbar

--- src/starter/navbar/NavBar.js
+++ src/starter/navbar/NavBar.js
@@ -1,28 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 
 import logo from "../../assets/icons/logo.svg";
 
 const NavBar = () => {
+  const [active, setActive] = useState(false);
+
+  const clickedLink = (e) => {
+    if (e.target.id !== active) {
+      setActive(e.target.id);
+    }
+  };
+
   return (
     <nav className="flex justify-between items-center">
-      <Link to="/" title="Home">
+      <Link to="/" title="Home" onClick={clickedLink}>
         <img className="logo" src={logo} alt="Kasa" />
       </Link>
 
       <div className="main-nav">
         <ul className="flex test">
           <li>
-            <NavLink to="/" className="main-color" title="Home">
+            <NavLink
+              id="home"
+              to="/"
+              className={`${active ? "" : "underline"} main-color`}
+              title="Home"
+              onClick={(e) => clickedLink(e)}
+            >
               ACCUEIL
             </NavLink>
           </li>
 
           <li>
             <NavLink
+              id="about"
               to="/about"
-              className="main-color no-underline"
+              className={`${active ? "underline" : ""} main-color`}
               title="About"
+              onClick={(e) => clickedLink(e)}
             >
               À PROPOS
             </NavLink>
